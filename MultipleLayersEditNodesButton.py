d2c5a1f7d245da8e979891ab7e5a5435dc8f4333
CARLOS EDUARDO CAGNA <carlos_cagna@yahoo.com.br>
2018-08-23 17:31:15 -0300

    Merge branch 'master' of https://github.com/CarlosCagna/MultipleLayersEditNodes

--- MultipleLayersEditNodesButton.py
+++ MultipleLayersEditNodesButton.py
@@ -15,6 +15,9 @@ from qgis.gui  import QgsMessageBar
 from PyQt4.QtCore import Qt, QSettings, QTranslator, qVersion, QCoreApplication, QFileInfo
 from PyQt4.QtGui import QMessageBox, QAction, QIcon, QProgressBar
 from PyQt4.QtSql import *
+import win32api, win32con
+import time
+
 
 
 class MultipleLayersEditNodesButton(QgsMapTool):
@@ -28,52 +31,77 @@ class MultipleLayersEditNodesButton(QgsMapTool):
         QgsMapTool.__init__(self, self.canvas)
         self.setCursor(Qt.CrossCursor)
         self.setAction(action)
+        
     
     def canvasPressEvent(self, e):
+    
+        def click(x,y):
+            win32api.SetCursorPos((x,y))
+            win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
+            time.sleep(0.005)
+            win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)     
+            time.sleep(0.005)    
+            win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
+            time.sleep(0.005)
+            win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)     
+            time.sleep(0.005)                
+            
+            print win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)  
+                
+            
         point = self.toLayerCoordinates(self.layer, e.pos())
         d = self.iface.mapCanvas().mapUnitsPerPixel()*4
         layer_select = 'NULL' 
-        for layer in iface.legendInterface().layers():            
-            if iface.legendInterface().isLayerVisible(layer):
-                if layer.geometryType()==2: 
+        select_line = False
+        select_polygon = False
+
+        count = 0
+        
+        
+        for layer in iface.legendInterface().layers():                
+            if iface.legendInterface().isLayerVisible(layer) and layer.type()==0:
+                
+                if layer.geometryType()==2 and layer.featureCount()>=count:
+                    count = layer.featureCount()
                     for feat in layer.getFeatures():
                         if feat.geometry() <> None: 
-                            if feat.geometry().intersects(QgsRectangle ((point.x()-d), (point.y()-d), (point.x()+d), (point.y()+d))):
-                                layer.select(feat.id())
-                                layer.startEditing()
-                                iface.legendInterface().setCurrentLayer(layer)
-                                iface.actionNodeTool().trigger()
+                            if feat.geometry().intersects(QgsRectangle ((point.x()-d), (point.y()-d), (point.x()+d), (point.y()+d))) and layer.name() =='Setor':
+                                layer_select = layer 
+                                select_polygon = True
+                            elif feat.geometry().intersects(QgsRectangle ((point.x()-d), (point.y()-d), (point.x()+d), (point.y()+d))) and select_polygon ==False:
                                 layer_select = layer 
                         
         for layer in iface.legendInterface().layers():            
-            if iface.legendInterface().isLayerVisible(layer):
+            if iface.legendInterface().isLayerVisible(layer) and layer.type()==0:
                 if layer.geometryType()==1: 
                     for feat in layer.getFeatures():
-                        if feat.geometry().intersects(QgsRectangle ((point.x()-d), (point.y()-d), (point.x()+d), (point.y()+d))):
-                            layer.select(feat.id())
-                            layer.startEditing()
-                            iface.legendInterface().setCurrentLayer(layer)
-                            iface.actionNodeTool().trigger()
-                            layer_select = layer 
+                        if feat.geometry() <> None:
+                            if feat.geometry().intersects(QgsRectangle ((point.x()-d), (point.y()-d), (point.x()+d), (point.y()+d))) and select_line == False:
+                                layer_select = layer 
+                                select_line = True
                              
         for layer in iface.legendInterface().layers():            
-            if iface.legendInterface().isLayerVisible(layer):
+            if iface.legendInterface().isLayerVisible(layer) and layer.type()==0:
                 if layer.geometryType()==0: 
                     for feat in layer.getFeatures():
-                        if feat.geometry().intersects(QgsRectangle ((point.x()-d), (point.y()-d), (point.x()+d), (point.y()+d))):
-                            layer.select(feat.id())
-                            layer.startEditing()
-                            iface.legendInterface().setCurrentLayer(layer)
-                            iface.actionNodeTool().trigger()
-                            layer_select = layer       
-        for layer in iface.legendInterface().layers():            
-            if layer <> layer_select:
-                layer.removeSelection()                
-
+                        if feat.geometry() <> None:
+                            if feat.geometry().intersects(QgsRectangle ((point.x()-d), (point.y()-d), (point.x()+d), (point.y()+d))):
+                                layer_select = layer       
+                            
+                
+        #layer.select(feat.id())
+        layer_select.startEditing()
+        iface.legendInterface().setCurrentLayer(layer_select)
+        iface.actionNodeTool().trigger()            
+                
+        select_line = False
+        x,y= win32api.GetCursorPos()    
+        click(x,y)		
+        
                         
     
     def deactivate(self):
-        if self is not None:
+        if self is not None:     
             QgsMapTool.deactivate(self)
         
     def activate(self):
